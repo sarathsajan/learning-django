9ac0e030ef761872a2d9575debbf1579fa4737c6
unknown <sarathsajan1998@gmail.com>
2021-09-24 00:55:47 +0530

    initialsed the polls app

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-55zilutg^nj(5qb25j*+)$3npwm&ox-*0qk1^jp0a^@$h!@8f-'
+SECRET_KEY = 'i_know_you_can_see_this_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
