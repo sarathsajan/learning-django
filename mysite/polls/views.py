1d789cfe26611840eba75d8c7ea93591c69a0f30
Sarath Sajan <sarathsajan1998@gmail.com>
2022-01-08 22:59:10 +0530

    converted views to generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,33 +3,27 @@ from django.http.response import HttpResponseRedirect
 from django.template import context, loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    template = loader.get_template("polls/index.html")
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return HttpResponse(template.render(context, request))
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -38,10 +32,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': "You didn't select a choice."})
     else:
         selected_choice.votes += 1
         selected_choice.save()
